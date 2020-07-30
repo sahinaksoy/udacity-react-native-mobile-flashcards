7effbb468f814219a0220a031dff25c6531fe18a
sahinaksoy <sahinaksoy@gmail.com>
2020-07-31 00:18:39 +0300

    style: add deck style

--- components/Deck.js
+++ components/Deck.js
@@ -1,17 +1,37 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const Deck = (props) => {
   return (
-    <View>
+    <View style={styles.deckBox}>
       <View>
-        <Text>Deck 1</Text>
+        <Text style={styles.deckText}>Deck 1</Text>
       </View>
       <View>
-        <Text>10 cards</Text>
+        <Text style={styles.cardText}>10 cards</Text>
       </View>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  deckBox: {
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: 100,
+    flexBasis: 100,
+    width:200,
+    backgroundColor: "#020122",
+    marginBottom: 8,
+    color: "#EDD382",
+  },
+  deckText: {
+    fontSize: 21,
+    color: "#EDD382",
+  },
+  cardText: {
+    fontSize: 18,
+    color: "#EDD382",
+  },
+});
 export default Deck;
